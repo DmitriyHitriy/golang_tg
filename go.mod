a4a313d80bf9cc246710cfe7890360c8005b0136
Dmitriy <stil4stil@ya.ru>
2023-09-21 14:48:49 +0300

    Получение сообщений с tdata аккаунта
    Получение сообщений с tdata аккаунта

--- go.mod
+++ go.mod
@@ -12,10 +12,15 @@ require (
 	github.com/gotd/td v0.87.0 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
+	go.opentelemetry.io/otel v1.18.0 // indirect
+	go.opentelemetry.io/otel/trace v1.18.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
+	rsc.io/qr v0.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/gotd/td/session"
 	"github.com/gotd/td/session/tdesktop"
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/query"
+	"github.com/gotd/td/telegram/query/dialogs"
+	"github.com/gotd/td/telegram/query/messages"
+	"github.com/gotd/td/tg"
 )
 
 func main() {
+	ctx := context.Background()
 	home, err := os.UserHomeDir()
 	if err != nil {
 		panic(err)
@@ -23,6 +31,41 @@ func main() {
 
 	data, err := session.TDesktopSession(accounts[0])
 	if err != nil {
+		panic(err)
+	}
+
+	var (
+		storage = new(session.StorageMemory)
+		loader  = session.Loader{Storage: storage}
+	)
+
+	if err := loader.Save(ctx, data); err != nil {
+		panic(err)
+	}
+
+	client := telegram.NewClient(1, "s", telegram.Options{SessionStorage: storage})
+
+	if err := client.Run(ctx, func(ctx context.Context) error {
+		raw := tg.NewClient(client)
+		cb := func(ctx context.Context, dlg dialogs.Elem) error {
+			// Skip deleted dialogs.
+			if dlg.Deleted() {
+				return nil
+			}
+			return dlg.Messages(raw).ForEach(ctx, func(ctx context.Context, elem messages.Elem) error {
+				msg, ok := elem.Msg.(*tg.Message)
+				if !ok {
+					return nil
+				}
+				peer := msg
+				fmt.Println(peer.Message)
+				time.Sleep(200 * time.Millisecond)
+				return nil
+			})
+		}
+
+		return query.GetDialogs(raw).ForEach(ctx, cb)
+	}); err != nil {
 		panic(err)
 	}
 
